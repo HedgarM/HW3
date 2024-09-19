ba52d9ce7e9e48f4b4a4bd301da701926927c5ac
Hedgar M <marinhedgar@yahoo.com>
2024-09-18 18:48:06 -0700

    Example 5 Completed

--- js/ex5.js
+++ js/ex5.js
@@ -1,17 +1,24 @@
-let numPasswordAttempts = 3;
+let maxPasswordAttempts =3;
+let numPasswordAttempts = 0;
 const correctPassword = "secret";
 
 
-while(numPasswordAttempts < 3)
+while(numPasswordAttempts < maxPasswordAttempts)
 {
   let password = prompt("Enter your password:")
 
   if(password === correctPassword)
   {
-
-    
-
+    console.log(`You entered the correct password after ${numPasswordAttempts} attempt(s)`)
+    break
   }
+  else
+  {
+    numPasswordAttempts ++;
+  }
+}
 
-
+if(numPasswordAttempts === maxPasswordAttempts)
+{
+console.log(`Your account is locked!  You failed to enter the correct password ${numPasswordAttempts} times`)
 }

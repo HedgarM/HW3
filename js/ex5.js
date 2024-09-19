81efe857dd7d3f29e057269cbbfb62d149e221a6
HedgarM <marinhedgar@yahoo.com>
2024-09-18 18:14:25 -0700

    Example 5 INP

---
+++ js/ex5.js
@@ -0,0 +1,17 @@
+let numPasswordAttempts = 3;
+const correctPassword = "secret";
+
+
+while(numPasswordAttempts < 3)
+{
+  let password = prompt("Enter your password:")
+
+  if(password === correctPassword)
+  {
+
+    
+
+  }
+
+
+}
